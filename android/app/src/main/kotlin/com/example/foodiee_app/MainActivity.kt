d0b06a0b9eeb71856d218fe54daed1864cdfbd6d
AslamThachapalli <aslam.develop912@gmail.com>
2022-09-12 11:44:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/foodiee_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodiee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Foodiee_app',
+      home: AuthScreen(),
+    );
+  }
+}
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
